e68c1643f56eb552b12ce42aaa46ff9d2a8078ac
Gareth Rushgrove <gareth@morethanseven.net>
2019-09-08 13:46:07 +0100

    Support Linuxbrew as well as Homebrew for Mac

--- render.rb
+++ render.rb
@@ -5,23 +5,28 @@ require "json"
 require "open-uri"
 
 PROJECT = "snyk/snyk"
-BIN = "snyk-macos"
+BIN = "snyk"
 
-api_url = "https://api.github.com/repos/#{PROJECT}/releases/latest"
+# Return the URL, SHA and version information for a GitHub release
+def get_latest_release(project, bin)
+  api_url = "https://api.github.com/repos/#{project}/releases/latest"
+  data = URI.parse(api_url).read
+  obj = JSON.parse(data)
+  version = obj["name"]
+  sha_url = "https://github.com/#{project}/releases/download/#{version}/#{bin}.sha256"
+  sha = URI.parse(sha_url).read
+  url = "https://github.com/#{project}/releases/download/#{version}/#{bin}"
+  sha256 = sha.split(" ").first
 
-data = URI.parse(api_url).read
-obj = JSON.parse(data)
+  return url, sha256, version
+end
 
-version = obj["name"]
-sha_url = "https://github.com/#{PROJECT}/releases/download/#{version}/#{BIN}.sha256"
 
-sha = URI.parse(sha_url).read
 
-@sha256 = sha.split(" ").first
-@version = version
-@url = "https://github.com/#{PROJECT}/releases/download/#{version}/#{BIN}"
+@url, @sha256, @version = get_latest_release(PROJECT, "#{BIN}-macos")
+@url_linux, @sha256_linux, _ = get_latest_release(PROJECT, "#{BIN}-linux")
 
 template = File.read("template.erb")
 
 renderer = ERB.new(template)
-puts output = renderer.result()
+puts renderer.result()
