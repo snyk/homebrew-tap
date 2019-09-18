a499e984854a5a9387afc1c37096a68e8de3d602
Gareth Rushgrove <gareth@morethanseven.net>
2019-09-18 00:03:37 +0000

    Upgraded to latest Snyk version skip-checks: true

--- Formula/snyk.rb
+++ Formula/snyk.rb
@@ -1,14 +1,14 @@
 class Snyk < Formula
   desc "Find & fix known vulnerabilities in open-source dependencies"
   homepage "https://github.com/snyk/snyk"
-  version "v1.227.0"
+  version "v1.228.0"
 
   if OS.mac?
-    url "https://github.com/snyk/snyk/releases/download/v1.227.0/snyk-macos"
-    sha256 "569300b2c4f7ccaefef0a396a8ac4af2962c990856d3e1e196cca5343a7e4379"
+    url "https://github.com/snyk/snyk/releases/download/v1.228.0/snyk-macos"
+    sha256 "30f8ec2fa2242da1b7eafad6c9d42385def4dae313226670671868075682d122"
   elsif OS.linux?
-    url "https://github.com/snyk/snyk/releases/download/v1.227.0/snyk-linux"
-    sha256 "74cbc68382759fac71845f86ae8321e85d352ea5584e70f2598ab1ff8e7a7c9c"
+    url "https://github.com/snyk/snyk/releases/download/v1.228.0/snyk-linux"
+    sha256 "e340f8e1d72b4c51ce77d1357e062278b8c153641145ec058d9e24542dd128af"
   end
 
   def install
