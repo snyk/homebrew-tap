05a19d8b62a499223c750078095f733af3f1d35a
Jakub Mikulas <jakub@snyk.io>
2021-07-15 11:19:25 +0200

    feat: use Snyk CDN for downloads instead of GH Releases

--- script/render.rb
+++ script/render.rb
@@ -16,27 +16,22 @@ require "erb"
 require "json"
 require "open-uri"
 
-PROJECT = "snyk/snyk"
 BIN = "snyk"
 
 # Return the URL, SHA and version information for a GitHub release
-def get_latest_release(project, bin)
-  api_url = "https://api.github.com/repos/#{project}/releases/latest"
-  data = URI.parse(api_url).read
+def get_latest_release(bin)
+  snyk_cli_version_url = "https://static.snyk.io/cli/latest/release.json"
+  data = URI.parse(snyk_cli_version_url).read
   obj = JSON.parse(data)
-  version = obj["name"]
-  sha_url = "https://github.com/#{project}/releases/download/#{version}/#{bin}.sha256"
-  sha = URI.parse(sha_url).read
-  url = "https://github.com/#{project}/releases/download/#{version}/#{bin}"
-  sha256 = sha.split(" ").first
+  version = obj["version"]
+  url = obj["assets"][bin]["url"]
+  sha256 = obj["assets"][bin]["sha256"].split.first
 
   return url, sha256, version
 end
 
-
-
-@url, @sha256, @version = get_latest_release(PROJECT, "#{BIN}-macos")
-@url_linux, @sha256_linux, _ = get_latest_release(PROJECT, "#{BIN}-linux")
+@url, @sha256, @version = get_latest_release("#{BIN}-macos")
+@url_linux, @sha256_linux, _ = get_latest_release("#{BIN}-linux")
 
 template = File.read(File.join(__dir__, "template.erb"))
 
