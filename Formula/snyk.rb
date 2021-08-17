0c1e3e459ee7a8fa152372f1ea295363b3862821
Jakub Mikulas <jakub@snyk.io>
2021-08-17 19:43:58 +0200

    Upgraded to latest Snyk version

--- Formula/snyk.rb
+++ Formula/snyk.rb
@@ -1,11 +1,11 @@
 class Snyk < Formula
   desc "Find & fix known vulnerabilities in open-source dependencies"
   homepage "https://github.com/snyk/snyk"
-  version "1.683.0"
+  version "1.683.1"
 
   if OS.mac?
-    url "https://static.snyk.io/cli/v1.683.0/snyk-macos"
-    sha256 "5b91cd249dee5f07c1bf5266a6eb248499074a16304e6283d2c052f013a7a935"
+    url "https://static.snyk.io/cli/dev/v1.683.1/snyk-macos"
+    sha256 "46a9fe4415861fccba710ead9ae2016a173fd559c774b50ce049e4fbdd67c8e9"
   elsif OS.linux?
     url "https://static.snyk.io/cli/v1.683.0/snyk-linux"
     sha256 "bae1b23cf512e44a31202ae27b7606228c9864fe7a4f3ccaf18cb025045bb9a8"
