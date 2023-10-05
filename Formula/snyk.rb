c645f530ae53c65274c1d8db44809e910f7147ba
Bastian Doetsch <bastian.doetsch@snyk.io>
2023-10-05 12:13:25 +0200

    feat: add arm64 binaries to formula [HEAD-867]

--- Formula/snyk.rb
+++ Formula/snyk.rb
@@ -1,18 +1,32 @@
 class Snyk < Formula
   desc "Find & fix known vulnerabilities in open-source dependencies"
   homepage "https://github.com/snyk/snyk"
-  version "1.1230.0"
+  version "1.1231.0"
 
-  if OS.mac?
-    url "https://static.snyk.io/cli/v1.1230.0/snyk-macos"
-    sha256 "cac4bf1113c624fac4de527084a806c9d43c1470f8bdedc95180007e70f5a2cb"
-  elsif OS.linux?
-    url "https://static.snyk.io/cli/v1.1230.0/snyk-linux"
-    sha256 "f63ecd7b5b809e6bb8c249e9f8724eba37c9c5a48732bd30cf62db5a16eaa467"
-  end
-
-  def install
-    bin.install (OS.linux? ? "snyk-linux" : "snyk-macos") => "snyk"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://static.snyk.io/cli/v1.1231.0/snyk-macos"
+    sha256 "378b8ae818229e73609cf621587c4223259595531c6b6f9c6d45a29468ef4910"
+    def install
+      bin.install ("snyk-macos") => "snyk"
+    end
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://static.snyk.io/cli/v1.1231.0/snyk-macos-arm64"
+    sha256 "0db6c23eaa975e1a73019f2e11c714aba386e043423cdb22ad41e4e6d51d9c07"
+    def install
+      bin.install ("snyk-macos-arm64") => "snyk"
+    end
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://static.snyk.io/cli/v1.1231.0/snyk-linux"
+    sha256 "0964f4fa4f3b5558484a8c2d726d9f7071f26a1a158fc88440828c860fe3c0c7"
+    def install
+      bin.install ("snyk-linux") => "snyk"
+    end
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://static.snyk.io/cli/v1.1231.0/snyk-linux-arm64"
+    sha256 "8b34e20cd3f038d5c355581cc660c4557c6c84508c20b856c877374af2ba5c12"
+    def install
+      bin.install ("snyk-linux-arm64") => "snyk"
+    end
   end
 
   test do
