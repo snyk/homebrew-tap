f2c0c5a8a78539989c65680f1011a5ac8e788572
Team Hammerhead <team-hammerhead@snyk.io>
2023-11-28 00:17:18 +0000

    Upgraded to latest Snyk version skip-checks: true

--- Formula/snyk.rb
+++ Formula/snyk.rb
@@ -1,29 +1,29 @@
 class Snyk < Formula
   desc "Find & fix known vulnerabilities in open-source dependencies"
   homepage "https://github.com/snyk/snyk"
-  version "1.1253.0"
+  version "1.1256.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://static.snyk.io/cli/v1.1253.0/snyk-macos"
-    sha256 "4a90b36e2b7422f368a5ad55591c3291d01a02994bdcdccdd0572c19be54b8d4"
+    url "https://static.snyk.io/cli/v1.1256.0/snyk-macos"
+    sha256 "ff331d6b27986572840f42eaf8bbd3d9fbeba59181f02ac3725aefb5435b8fab"
     def install
       bin.install ("snyk-macos") => "snyk"
     end
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://static.snyk.io/cli/v1.1253.0/snyk-macos-arm64"
-    sha256 "14e216f9ffb506654a59b14d85456492ece965d7d15c73c3ef82f926290bdb8b"
+    url "https://static.snyk.io/cli/v1.1256.0/snyk-macos-arm64"
+    sha256 "346a52114f682f176536740e9e972758e2bfd678c7e8da30bb99058e19afb276"
     def install
       bin.install ("snyk-macos-arm64") => "snyk"
     end
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://static.snyk.io/cli/v1.1253.0/snyk-linux"
-    sha256 "4bd6314c9562cd9cc46822da8004f89ac1b14b682bd3b33a980561891d15fd5f"
+    url "https://static.snyk.io/cli/v1.1256.0/snyk-linux"
+    sha256 "aea12f014c89b171d0230da472b6c46557ffc3cc3c06abd1a354cdfc87dedb79"
     def install
       bin.install ("snyk-linux") => "snyk"
     end
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://static.snyk.io/cli/v1.1253.0/snyk-linux-arm64"
-    sha256 "f1da3d6492d53b26155a521de0bdfeea5f057e545df90b956ec0ffbf5d599f86"
+    url "https://static.snyk.io/cli/v1.1256.0/snyk-linux-arm64"
+    sha256 "58876031aa6344d6405d84a6a1f47b4afa11e3cc32804e698cd43a5a89f8ab43"
     def install
       bin.install ("snyk-linux-arm64") => "snyk"
     end
