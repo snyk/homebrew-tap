81dad7844d95f99b4fceab6f3bef0935e32f9b45
Team Hammerhead <team-cli@snyk.io>
2024-12-13 00:24:36 +0000

    Upgraded to latest Snyk version skip-checks: true

--- Formula/snyk.rb
+++ Formula/snyk.rb
@@ -1,29 +1,29 @@
 class Snyk < Formula
   desc "Find & fix known vulnerabilities in open-source dependencies"
   homepage "https://github.com/snyk/snyk"
-  version "1.1294.2"
+  version "1.1294.3"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://downloads.snyk.io/cli/v1.1294.2/snyk-macos?utm_source=HOMEBREW"
-    sha256 "62227bc4a9b4fb3fe047fa788a2b3b37f4c8138cf77f4ac0ad0989134178120c"
+    url "https://downloads.snyk.io/cli/v1.1294.3/snyk-macos?utm_source=HOMEBREW"
+    sha256 "ee15fc0d27c1a1b6439bf40595876cdde89d2ed615c935b9081ea7d2962db16c"
     def install
       bin.install ("snyk-macos") => "snyk"
     end
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://downloads.snyk.io/cli/v1.1294.2/snyk-macos-arm64?utm_source=HOMEBREW"
-    sha256 "750a4ba317d0efcb193e6ab3f4e7c8286a2c21ed025de64890e72354018c943f"
+    url "https://downloads.snyk.io/cli/v1.1294.3/snyk-macos-arm64?utm_source=HOMEBREW"
+    sha256 "bda9b115ba6331e014bfa6d4cbd4574ef69f0047b3c4b1db27bfb70bfe2a1cfe"
     def install
       bin.install ("snyk-macos-arm64") => "snyk"
     end
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://downloads.snyk.io/cli/v1.1294.2/snyk-linux?utm_source=HOMEBREW"
-    sha256 "c913279efd0ee37c84fad43c239bd13494ab891db7e12e2dc984ff4309743cef"
+    url "https://downloads.snyk.io/cli/v1.1294.3/snyk-linux?utm_source=HOMEBREW"
+    sha256 "cfb789e412a29c895472eabbd3dce3c8e72b6286691c1e42e80b0a01c7db064f"
     def install
       bin.install ("snyk-linux") => "snyk"
     end
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://downloads.snyk.io/cli/v1.1294.2/snyk-linux-arm64?utm_source=HOMEBREW"
-    sha256 "28c6b1f9a87a9a9c120ba79b6a39c72c95afbdab72d75fac520d9d0efedab963"
+    url "https://downloads.snyk.io/cli/v1.1294.3/snyk-linux-arm64?utm_source=HOMEBREW"
+    sha256 "dfc05303098b0e1b78ca9ae62022d2f50bf532d94658cb9e70ed5b8fc7ee4a88"
     def install
       bin.install ("snyk-linux-arm64") => "snyk"
     end
